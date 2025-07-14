de71d2e7b7c2fd12d8f91dcb3cfcbd488f5d5873
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-07-14 01:13:06 -0300

    add som core exceptions

---
+++ src/core/exceptions/payment_exceptions.py
@@ -0,0 +1,9 @@
+from ..shared.exception_base import ExceptionBase
+
+
+class PaymentNotFoundInPurchaseException(ExceptionBase):
+    '''Exception raised when a payment is not found in a purchase.'''
+
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.code = 'PAYMENT_NOT_FOUND_IN_PURCHASE_EXCEPTION'

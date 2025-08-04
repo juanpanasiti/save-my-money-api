3f432ba48ac3bc9ac7d057cfce703564313a6eb3
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-08-03 22:19:27 -0300

    add barrel and fix model

---
+++ src/core/expense/models/__init__.py
@@ -0,0 +1,13 @@
+from .expense import Expense
+from .expense_category import ExpenseCategory
+from .payment import Payment
+from .purchase import Purchase
+from .subscription import Subscription
+
+__all__ = [
+    'Expense',
+    'ExpenseCategory',
+    'Payment',
+    'Purchase',
+    'Subscription',
+]
--- src/core/expense/models/expense_category.py
+++ src/core/expense/models/expense_category.py
@@ -2,11 +2,13 @@ from uuid import UUID
 from typing import Optional
 
 from ...shared.entity_base import EntityBase
+from ...user import User
 
 
 class ExpenseCategory(EntityBase):
     def __init__(
         self,
+        owner: User,
         name: str,
         description: Optional[str] = None,
         is_income: bool = False,
@@ -16,6 +18,7 @@ class ExpenseCategory(EntityBase):
         self._name = name
         self._description = description
         self._is_income = is_income
+        self._owner_id = owner.id
 
     @property
     def name(self) -> str:
@@ -38,11 +41,12 @@ class ExpenseCategory(EntityBase):
     def description(self, value: Optional[str]):
         'Set the description of the expense category.'
         self._description = value
-    
+
     @property
     def is_income(self) -> bool:
         'Check if the category is for income.'
         return self._is_income
+
     @is_income.setter
     def is_income(self, value: bool):
         'Set whether the category is for income.'
@@ -50,11 +54,17 @@ class ExpenseCategory(EntityBase):
             raise ValueError('is_income must be a boolean value')
         self._is_income = value
 
+    @property
+    def owner_id(self) -> UUID:
+        'Get the ID of the owner of the expense category.'
+        return self._owner_id
+
     @classmethod
     def from_dict(cls, data: dict) -> 'ExpenseCategory':
         '''Create an ExpenseCategory instance from a dictionary representation.'''
         return cls(
             name=data['name'],
+            owner=User(**data['owner']),
             description=data.get('description'),
             is_income=data.get('is_income', False),
             id=data.get('id')
